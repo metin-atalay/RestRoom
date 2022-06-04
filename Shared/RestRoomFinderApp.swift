4006c09553de04daaf3eb633e0c1d19e393c337d
Metin Atalay <metin.atalay@trtworld.com>
2022-06-04 14:58:56 +0300

    Initial Commit

---
+++ Shared/RestRoomFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestRoomFinderApp.swift
+//  Shared
+//
+//  Created by Metin Atalay on 4.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestRoomFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
